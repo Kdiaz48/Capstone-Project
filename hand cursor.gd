3abe0c82e0cea8e03bc4c882cdd2f2825d4623fd
dsantiago19 <dsantiago19@fordham.edu>
2024-04-29 00:33:32 -0400

    up

---
+++ hand cursor.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var hand_cursor = preload("res://Ai_Cursor_Open.png")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func change_cursor_hand():
+	Input.set_custom_mouse_cursor(hand_cursor)
+	
+func change_cursor_back():
+	Input.set_custom_mouse_cursor(null)

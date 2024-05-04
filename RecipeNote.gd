c8aa8b7c2b4bae55c6277b03f50e9e55db31afd5
Kdiaz48 <kdiaz48@fordham.edu>
2024-05-03 23:26:50 -0400

    Warehouse and Mansion Dialogue

--- RecipeNote.gd
+++ RecipeNote.gd
@@ -8,4 +8,4 @@ func _on_back_to_scene_pressed():
 
 
 func _on_go_to_warehouse_pressed():
-	get_tree().change_scene_to_file("res://warehouseSCENE.tscn")
+	get_tree().change_scene_to_file("res://ArrivalAtWarehouse.tscn")
---
+++ WarehouseCoordinates.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://warehouseSCENE.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://bathroom_1.tscn")

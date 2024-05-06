ac023c0ae84397d3a79ceffa64d9432244b713a5
Kdiaz48 <kdiaz48@fordham.edu>
2024-05-06 16:07:25 -0400

    Closet and Study

---
+++ CLOSETCode1.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://CLOSETCodeWrong.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://CLOSETCodeWrong.tscn")
